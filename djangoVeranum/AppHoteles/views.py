ead5245631ec51371b20623188328b110f84337e
0snola <alonso7gonz@gmail.com>
2021-06-25 21:07:51 -0400

    ayuda

--- djangoVeranum/AppHoteles/views.py
+++ djangoVeranum/AppHoteles/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from djangoVeranum.AppHoteles.models import ReservaHabitacion
+from django.shortcuts import redirect, render
+from AppHoteles.models import *
+from AppHoteles.forms import HabitacionForm
 
 # Create your views here.
 def home (request):
     return render(request,'principal.html')
+
+def reservaHabitacion(request):
+    if request.method == "POST":
+        form = HabitacionForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/reservas')
+            except:
+                pass
+    else:
+        form = HabitacionForm()
+        reservaHabitacion = ReservaHabitacion.objects.all()
+        return render(request,'mantenedorReservas.html',{'form': form, 'reservaHabitacion': reservaHabitacion})
--- djangoVeranum/djangoVeranum/urls.py
+++ djangoVeranum/djangoVeranum/urls.py
@@ -19,5 +19,5 @@ from AppHoteles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hoteles/', views.home)
+    path('hoteles/', views.reservaHabitacion,name="reservaHabitacion"),
 ]
